e64fd55582d4ad4f1d0a25c246465cb9317cf371
izzaz133 <izzazahmed134@gmail.com>
2022-11-27 18:51:16 +0530

    common words fn

--- src/App.jsx
+++ src/App.jsx
@@ -7,63 +7,80 @@ function App() {
   const [youtubeURL, setYoutubeURL] = useState('')
   const [youtubeLinks, setYoutubeLinks] = useState([])
   const [youtubeId, setYoutubeId] = useState([])
-
+  const [commonTags, setCommonTags] = useState([])
 
 
   const addYoutubeLinks = () => {
     setYoutubeLinks(links => [...links, youtubeURL])
     setYoutubeURL('')
-   
+
     const extId = extractId(youtubeURL)
-    setYoutubeId(id => [...id, extId ])
-  
-   
+    setYoutubeId(id => [...id, extId])
   }
-  
 
 
   const extractVidTags = async () => {
-    
-    youtubeId.forEach (async (link) => {
+
+    youtubeId.forEach(async (link) => {
       const extTags = await extractTags(link)
       console.log(extTags)
-      setYoutubeTags(id => [...id, extTags ])
+      setYoutubeTags(id => [...id, extTags])
     })
-   
-
-    
-    // const tags = await extractTags(id)
-   
-    // setYoutubeTags(tags)
-    
   }
 
   const extractId = (url) => {
     return url.split('/').pop()
   }
 
+
+  const compareTags = () => {
+    console.log('compare')
+    try {
+      const com = youtubeTags.reduce((p,c) => p.filter(e => c.some(el => el.toLowerCase() === e.toLowerCase())));
+      console.log(com)
+      setCommonTags(com)  
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div className="App">
       <h1>YOUTUBE TAG EXTRACTOR</h1>
       <div>
-        <input type="text" placeholder='Enter Youtube URL' value={youtubeURL} onChange={(e) => setYoutubeURL(e.target.value)} style={{ width:'600px', marginBottom:'20px', padding:'10px 5px' }} />
-        <button onClick={addYoutubeLinks}>Add</button>
+        <input type="text" placeholder='Enter Youtube URL' value={youtubeURL} onChange={(e) => setYoutubeURL(e.target.value)} style={{ width: '600px', marginBottom: '20px', padding: '10px 5px' }} />
+        <button onClick={addYoutubeLinks}>Add URL</button>
       </div>
-      {
-        youtubeLinks.map((link,index) => (
-          <p key={link}>{index+1}. {link}</p>
-        ))
-      }
+      <div style={{ textAlign: "left" }}>
+        {
+          youtubeLinks.map((link, index) => (
+            <p key={link}>{index + 1}. {link}</p>
+          ))
+        }
+      </div>
 
 
       <button onClick={extractVidTags}>GET VIDEO TAG</button>
 
-      <h2>ALL Tags</h2>
+      {youtubeTags.length != 0 ? <h2>ALL Tags</h2> : ''}
       {
-        youtubeTags.map((tag,index) => (
+        youtubeTags.map((tag, index) => (
           <textarea name="" id="" cols="100" rows="10" key={index} value={tag} readOnly></textarea>
         ))
       }
+      <div>
+      {youtubeTags.length != 0 ? 
+       <button onClick={compareTags}>Find Common Tags</button>
+       : 
+       ''}
+      </div>
+
+       {
+        commonTags.map((tag,index) => (
+          <textarea name="" id="" cols="100" rows="10" key={index} value={tag} readOnly></textarea>
+        ))
+       }
+ 
     </div>
   )
 }
